4d9c5f116bccd5032d068a54d137e6fbddf09bca
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-07-23 23:38:35 +0300

    Task 4 : Add all unversioned files

---
+++ api-gateway/src/test/java/com/springbootmicroservices/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmicroservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ user-service/src/main/java/com/springbootmicroservices/userservice/UserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmicroservices.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserserviceApplication.class, args);
+    }
+
+}
