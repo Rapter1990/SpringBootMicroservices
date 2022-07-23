5c90c97863ef78c113b90fcb6e2311fb739aee4f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-07-23 23:40:35 +0300

    Task 5 : Fix some code issues in Api Gateway

--- api-gateway/src/main/java/com/springbootmicroservices/apigateway/ApigatewayApplication.java
+++ api-gateway/src/main/java/com/springbootmicroservices/apigateway/ApigatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ApiGatewayApplication {
+public class ApigatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(ApigatewayApplication.class, args);
     }
 
 }
