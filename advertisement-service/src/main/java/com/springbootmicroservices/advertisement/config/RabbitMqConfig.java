470a18e9c4910248be9479e343391eccfbe12117
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-07-25 15:36:12 +0300

    Task 17 : Revise RabbitMqConfig file in Management Service

--- advertisement-service/src/main/java/com/springbootmicroservices/advertisement/config/RabbitMqConfig.java
+++ advertisement-service/src/main/java/com/springbootmicroservices/advertisement/config/RabbitMqConfig.java
@@ -7,13 +7,16 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class RabbitMqConfig {
 
-    @Value("${spring.rabbitmq.queue.name}")
+    @Value("${queue.name}")
     private String queueName;
 
     @Value("${spring.rabbitmq.template.exchange}")
