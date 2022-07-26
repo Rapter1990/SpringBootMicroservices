4682a4f19861e0e6b6664b43592198d9f6a15093
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-07-26 23:57:17 +0300

    Task 27 : Change method type of infoUser method in User service

--- user-service/src/main/java/com/springbootmicroservices/userservice/controller/UserController.java
+++ user-service/src/main/java/com/springbootmicroservices/userservice/controller/UserController.java
@@ -16,10 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -61,7 +58,7 @@ public class UserController {
     }
 
 
-    @PostMapping("/info")
+    @GetMapping("/info")
     public ResponseEntity<?> infoUser(){
 
         LOGGER.info("UserController | infoUser is started");
