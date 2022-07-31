851d29bf82f04e8e0294f291bdae3378819dd0f6
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-07-31 18:33:41 +0300

    Task 39 : Fix the issue not getting message from advertisement service through RabbitListener in Report Service

--- report-service/src/main/java/com/springbootmicroservices/report/service/MessageService.java
+++ report-service/src/main/java/com/springbootmicroservices/report/service/MessageService.java
@@ -1,6 +1,6 @@
 package com.springbootmicroservices.report.service;
 
-import com.springbootmicroservices.report.dto.AdvertisementDto;
+import com.springbootmicroservices.advertisement.dto.AdvertisementDto;
 
 public interface MessageService {
     void receiveMessage(AdvertisementDto advertisementDto);
