5f55dd45e092aaf973189a3abf8a23c9e3cce939
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-08-19 17:08:23 +0300

    Task 63 : Update docker-compose.yml and init.sql

--- init/init.sql
+++ init/init.sql
@@ -5,6 +5,10 @@ CREATE DATABASE IF NOT EXISTS `springbootuser`;
 -- CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
 -- GRANT ALL ON *.* TO 'root'@'%';
 
+-- GRANT ALL PRIVILEGES ON `springbootadvertisement`.* TO 'springmicroserviceuser'@'%' WITH GRANT OPTION;
+-- GRANT ALL PRIVILEGES ON `springbootreport`.* TO 'springmicroserviceuser'@'%' WITH GRANT OPTION;
+-- GRANT ALL PRIVILEGES ON `springbootuser`.* TO 'springmicroserviceuser'@'%' WITH GRANT OPTION;
+
 GRANT ALL ON `springbootadvertisement`.* TO 'springmicroserviceuser'@'%';
 GRANT ALL ON `springbootreport`.* TO 'springmicroserviceuser'@'%';
 GRANT ALL ON `springbootuser`.* TO 'springmicroserviceuser'@'%';
